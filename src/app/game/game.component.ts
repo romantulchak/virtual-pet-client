4d4605501a130dcf93c528cabfa8c6412869c2e9
romantulchak <romantulchak29@gmail.com>
2020-11-20 20:50:57 +0100

    Added: health bar in the battle

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,11 @@
-import { Component, HostListener, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { send } from 'process';
 import { Subscription } from 'rxjs';
@@ -25,23 +32,23 @@ import WebGLRenderer = THREE.WebGLRenderer;
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit, OnDestroy {
-
   public currentHero: SubHero;
   private money: number = 0;
-  private moneyInterval:any;
+  private moneyInterval: any;
   private sendInterval: any;
   public isUpdatePrice: boolean = false;
   public errorMessage: string;
   public currentBoss: any;
   public isUpdateAttack: boolean = false;
   public level: number;
-  constructor(private gameService: GameService) {  }
+  public bossHealth: number;
+  constructor(private gameService: GameService) { }
   /*constructor(private gameService: GameService, private render: Renderer2) {  }*/
 
-/*
+  /*
   @ViewChild("myCanvas") myCanvas:any;
   private path:string = '../../assets/images/game/Squirtle.stl';
   private scene: Scene;
@@ -52,117 +59,107 @@ export class GameComponent implements OnInit, OnDestroy {
 
   @HostListener('window:beforeunload', ['$event'])
   beforeUnloadHandler(event: any) {
-  event.preventDefault();
-  this.sendMoneyPerMinute();
-  return false;
-}
-ngAfterViewInit(): void {
-//  this.init3D();
-}
+    event.preventDefault();
+    this.sendMoneyPerMinute();
+    return false;
+  }
+  ngAfterViewInit(): void {
+    //  this.init3D();
+  }
   ngOnInit(): void {
     /*let global = this.render.listen('window', 'resize', (evt) => {
       this.onWindowResize();
     });*/
-    this.gameService.currentHero.subscribe(
-      res=>{
-        this.currentHero = res;
+    this.gameService.currentHero.subscribe((res) => {
+      this.currentHero = res;
+    });
+    this.getBoss();
 
-      }
-    );
-      this.getBoss();
-
-      if(this.currentHero != null){
-
-        this.moneyEverySecond();
-        this.sendInterval = setInterval(()=>{
-          this.sendMoneyPerMinute();
-        },60000);
-      }
-
+    if (this.currentHero != null) {
+      this.moneyEverySecond();
+      this.sendInterval = setInterval(() => {
+        this.sendMoneyPerMinute();
+      }, 60000);
+    }
   }
 
-  private getBoss(){
-    this.gameService.getBoss(this.subReq()).subscribe(
-      res=>{
-        this.currentBoss = res.boss;
-        this.level = res.level;
-      }
-    );
+  private getBoss() {
+    this.gameService.getBoss(this.subReq()).subscribe((res) => {
+      this.currentBoss = res.boss;
+      this.level = res.level;
+
+      this.bossHealth = this.currentBoss.health;
+    });
   }
 
-  private moneyEverySecond(){
-  this.moneyInterval = setInterval(()=>{
-        this.money += this.currentHero.moneyMultiplier;
-        this.updateBoss();
+  private moneyEverySecond() {
+    this.moneyInterval = setInterval(() => {
+      this.money += this.currentHero.moneyMultiplier;
+      this.updateBoss();
 
-        this.currentHero.currency.money += this.currentHero.moneyMultiplier;
+      this.currentHero.currency.money += this.currentHero.moneyMultiplier;
     }, 1000);
-
   }
-  public upAttackLevel(){
+  public upAttackLevel() {
     this.sendMoneyPerMinute();
-    this.isUpdateAttack= true;
+    this.isUpdateAttack = true;
     setTimeout(() => {
-      this.gameService.upAttackLevel(this.subReq()).subscribe(
-        res=>{
-          if(res.httpStatus == 'OK' && this.currentHero.currency.money >= this.currentHero.subAttack.attackMoneyUp){
-            this.currentHero = res.subDTO;
-            }else{
-            this.errorMessage = "You do not have enough money";
-          }
-          this.isUpdateAttack = false;
+      this.gameService.upAttackLevel(this.subReq()).subscribe((res) => {
+        if (
+          res.httpStatus == 'OK' &&
+          this.currentHero.currency.money >=
+          this.currentHero.subAttack.attackMoneyUp
+        ) {
+          this.currentHero = res.subDTO;
+        } else {
+          this.errorMessage = 'You do not have enough money';
         }
-      );
+        this.isUpdateAttack = false;
+      });
     }, 1000);
   }
-  public upMoneyLevel(){
+  public upMoneyLevel() {
     this.sendMoneyPerMinute();
     this.isUpdatePrice = true;
     setTimeout(() => {
-      this.gameService.upMoneyLevel(this.subReq()).subscribe(
-        res=>{
-
-          if(res.httpStatus == 'OK' && this.currentHero.currency.money >= this.currentHero.moneyUpPrice){
-            this.currentHero = res.subDTO;
-
-          }else{
-            this.errorMessage = "You do not have enough money";
-          }
-          this.isUpdatePrice = false;
-
-        }
-      );
-
-    }, 1000);
-
-  }
-
-  private sendMoneyPerMinute(){
-
-      this.gameService.sendMoney(this.subReq(), this.money).subscribe(
-        res=>{
+      this.gameService.upMoneyLevel(this.subReq()).subscribe((res) => {
+        if (
+          res.httpStatus == 'OK' &&
+          this.currentHero.currency.money >= this.currentHero.moneyUpPrice
+        ) {
           this.currentHero = res.subDTO;
-          this.money = 0;
+        } else {
+          this.errorMessage = 'You do not have enough money';
         }
-      );
+        this.isUpdatePrice = false;
+      });
+    }, 1000);
   }
 
-  private updateBoss(){
+  private sendMoneyPerMinute() {
+    this.gameService.sendMoney(this.subReq(), this.money).subscribe((res) => {
+      this.currentHero = res.subDTO;
+      this.money = 0;
+    });
+  }
+
+
+  private updateBoss() {
     this.currentBoss.health -= this.currentHero.attack;
 
-    if(this.currentBoss.health <= 0){
+    if (this.currentBoss.health <= 0) {
       this.money += this.currentBoss.droppedMoney;
       this.sendMoneyPerMinute();
       this.getBoss();
     }
   }
 
-  public moneyClikcs(){
+  public moneyClikcs() {
     this.money += this.currentHero.moneyMultiplier;
     this.currentHero.currency.money += this.currentHero.moneyMultiplier;
     this.updateBoss();
   }
-  private subReq():SubRequest{
+  private subReq(): SubRequest {
     let subRequest = new SubRequest();
     subRequest.name = this.currentHero.name;
     subRequest.subId = this.currentHero.id;
@@ -171,11 +168,11 @@ ngAfterViewInit(): void {
   }
 
   ngOnDestroy(): void {
-      clearInterval(this.moneyInterval);
-      this.sendMoneyPerMinute();
-      clearInterval(this.sendInterval);
-
+    clearInterval(this.moneyInterval);
+    this.sendMoneyPerMinute();
+    clearInterval(this.sendInterval);
   }
+
   /*
   private init3D(){
     console.log(this.myCanvas.nativeElement);
